41bc07a088c60a28e591ae800cf47ba035804fec
Hunter19823 <hspragg@asu.edu>
2023-11-22 15:51:56 -0700

    Added a logo.

--- common/src/main/java/pie/ilikepiefoo/moddingtemplate/ModdingTemplate.java
+++ common/src/main/java/pie/ilikepiefoo/moddingtemplate/ModdingTemplate.java
@@ -2,6 +2,7 @@ package pie.ilikepiefoo.moddingtemplate;
 
 public class ModdingTemplate {
     public static final String MOD_ID = "moddingtemplate";
+    public static final String MOD_NAME = "Modding Template";
 
     public static void init() {
 
